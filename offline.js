3d955ca4c48e9a75d453de75776a9d7befd45f66
nait <nait@gmail.com>
2025-04-26 12:07:53 +0700

    phone test2

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1745048425,
+	"version": 1745643906,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -96,6 +96,7 @@
 		"images/sprite10-sheet0.png",
 		"images/sprite11-sheet0.png",
 		"images/sprite12-sheet0.png",
+		"images/sprite13-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
